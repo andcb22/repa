3e1ce57f8af47b4a8e519c0414029fb862aa5ca4
andcb22 <andcb@tut.by>
2021-11-24 16:26:49 +0300

    Сделано задание 5 и тестировано

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,6 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+#split по пробелу, последний элемент, split по запятой, перевести в множество, пересечение и сортировка
+result = sorted(list(set(command1.split()[-1].split(','))&set(command2.split()[-1].split(','))))
+print(result)
