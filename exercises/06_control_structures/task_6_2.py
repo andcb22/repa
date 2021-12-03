9579039d7960df52624d44366c44193d953b641c
andcb22 <andcb@tut.by>
2021-12-03 16:15:01 +0300

    Сделано 6_2 и тест

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -7,8 +7,20 @@
    'unicast' - если первый байт в диапазоне 1-223
    'multicast' - если первый байт в диапазоне 224-239
    'local broadcast' - если IP-адрес равен 255.255.255.255
-   'unassigned' - если IP-адрес равен 0.0.0.0
+   'unassigned' - если IP-адрес равен 0
    'unused' - во всех остальных случаях
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip=input('Введите IP-адрес в формате 10.0.1.1 :')
+ip_list=ip.split('.')
+if ip == '0.0.0.0':
+    print('unassigned')
+elif ip == '255.255.255.255':
+    print('local broadcast')
+elif int(ip_list[0]) in range(1,223):
+    print('unicast')
+elif int(ip_list[0]) in range(224,239):
+    print('multicast')
+else:
+    print('unused')
