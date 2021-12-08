125537f17df305b01403907f41d6649ee4cb3d40
andcb22 <andcb@tut.by>
2021-12-08 15:12:15 -0600

    workon 6_2b

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,43 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip=input('Введите IP-адрес в формате 10.0.1.1 :')
+
+ip_list = ip.split('.')
+a_correct=True
+
+while True:
+   if not len(ip_list) == 4:
+       a_correct=False
+   if a_correct:
+       if not ip_list[0].isdigit():
+           a_correct=False
+       if not ip_list[1].isdigit():
+           a_correct=False
+       if not ip_list[2].isdigit():
+           a_correct=False
+       if not ip_list[3].isdigit():
+           a_correct=False
+   if a_correct:
+       if not (int(ip_list[0]) >= 0 and int(ip_list[0]) <= 255):
+           a_correct=False
+       if not (int(ip_list[1]) >= 0 and int(ip_list[1]) <= 255):
+           a_correct=False
+       if not (int(ip_list[2]) >= 0 and  int(ip_list[2]) <= 255):
+           a_correct=False
+       if not (int(ip_list[3]) >= 0 and  int(ip_list[3]) <= 255):
+           a_correct=False
+
+   if a_correct:
+       if ip == '0.0.0.0':
+            print('unassigned')
+    elif ip == '255.255.255.255':
+        print('local broadcast')
+    elif int(ip_list[0]) in range(1, 223):
+        print('unicast')
+    elif int(ip_list[0]) in range(224, 239):
+        print('multicast')
+    else:
+        print('unused')
+else:
+    print('Неправильный IP-адрес')
