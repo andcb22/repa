e8e7b935a4fa87b5c337f9919fb0364f2081bd0a
andcb22 <andcb@tut.by>
2021-12-24 14:47:57 +0300

    workon 7_3b

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,19 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+with open('CAM_table.txt','r') as f:
+    line_list=[]
+    for line in f:
+        line=line.strip()
+        if line and line.split()[0][0].isdigit():
+            line_list.append([int(line.split()[0]), line.split()[1], line.split()[3]]) 
+    line_list.sort()
+#    for line in line_list:
+#        print('{:<8}{:20}{}'.format(line[0],line[1],line[2]))
+vlan = input('Enter VLAN number: ')
+for line in line_list:
+    if vlan == line[0]
+        print('{:<8}{:20}{}'.format(line[0],line[1],line[2]))
+
+
+
