94af5b07bd408f6d0cf05eab67b2b5b087fded9c
andcb22 <andcb@tut.by>
2021-12-28 14:15:16 -0600

    Сделано упр 7_3b и тест

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -23,12 +23,9 @@ with open('CAM_table.txt','r') as f:
         line=line.strip()
         if line and line.split()[0][0].isdigit():
             line_list.append([int(line.split()[0]), line.split()[1], line.split()[3]]) 
-    line_list.sort()
-#    for line in line_list:
-#        print('{:<8}{:20}{}'.format(line[0],line[1],line[2]))
 vlan = input('Enter VLAN number: ')
 for line in line_list:
-    if vlan == line[0]
+    if int(vlan) == line[0]:
         print('{:<8}{:20}{}'.format(line[0],line[1],line[2]))
 
 
