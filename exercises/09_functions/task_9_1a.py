30df5c453c17eafb362839b2ad23141005103a9b
andcb22 <andcb@tut.by>
2021-12-29 11:45:56 +0300

    Сделано упр 9_1а и тест

--- exercises/09_functions/task_9_1a.py
+++ exercises/09_functions/task_9_1a.py
@@ -43,3 +43,28 @@ port_security_template = [
 ]
 
 access_config = {"FastEthernet0/12": 10, "FastEthernet0/14": 11, "FastEthernet0/16": 17}
+
+def generate_access_config(intf_vlan_mapping, access_template, psecurity=None):
+    """
+    intf_vlan_mapping - словарь с соответствием интерфейс-VLAN такого вида:
+        {'FastEthernet0/12':10,
+         'FastEthernet0/14':11,
+         'FastEthernet0/16':17}
+    access_template - список команд для порта в режиме access
+
+    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
+    """
+    command_list=[]
+    for intf,vlan in intf_vlan_mapping.items():
+      command_list.append(f"interface {intf}")
+      for command in access_template:
+        if command.endswith('vlan'):
+          command_list.append(f"{command} {vlan}")
+        else:
+          command_list.append(f"{command}")
+      if psecurity:
+         for psec_command in psecurity:
+             command_list.append(psec_command)
+    return command_list
+print(generate_access_config(access_config, access_mode_template, port_security_template))
+#
