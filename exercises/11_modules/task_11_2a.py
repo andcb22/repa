8ee446ee6f5cf32aad941d6f2cce5fc1180d20fc
andcb22 <andcb@tut.by>
2022-01-18 16:59:49 +0300

    workon 11_2a

--- exercises/11_modules/task_11_2a.py
+++ exercises/11_modules/task_11_2a.py
@@ -73,10 +73,16 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
-
+from task_11_2 import create_network_map
+from draw_network_graph import draw_topology
 infiles = [
     "sh_cdp_n_sw1.txt",
     "sh_cdp_n_r1.txt",
     "sh_cdp_n_r2.txt",
     "sh_cdp_n_r3.txt",
 ]
+
+
+if __name__ == "__main__":
+    print(create_network_map(infiles))
+
