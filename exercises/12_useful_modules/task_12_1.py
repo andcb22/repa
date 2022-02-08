77ec0e146712bd09caea65729dc52a0615d5eead
andcb22 <andcb@tut.by>
2022-02-08 14:51:53 +0300

    Сделвно упр. 12_1 и тест

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,25 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+#import ipaddress
+import subprocess
+
+ip_list=['8.8.8.8','8.8.4.4','a']
+
+def ping_ip_addresses(ip_list):
+    ip_list_good=[]
+    ip_list_bad=[]
+    for ip in ip_list:
+        reply=subprocess.run(['ping','-c','3',ip])
+        if reply.returncode == 0:
+            ip_list_good.append(ip)
+        else:
+            ip_list_bad.append(ip)
+    pass
+    return(ip_list_good,ip_list_bad)
+
+if __name__ == "__main__":
+    result=ping_ip_addresses(ip_list)
+    print(type(result))
+    print(result)
+
