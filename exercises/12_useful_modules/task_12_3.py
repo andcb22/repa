6dc65b41b4c4a0c1c89cc60d325bb786beff70f8
andcb22 <andcb@tut.by>
2022-02-23 16:58:56 +0300

    12_3 working

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -18,3 +18,13 @@ Reachable    Unreachable
              10.1.1.9
 
 """
+from tabulate import tabulate
+ip_unreachable=['1.2.3.a','1.2.3.b','1.22.3.c']
+ip_reachable=['1.1.1.1','1.1.1.2']
+def print_ip_table(ip_reachable,ip_unreachable):
+    headers=('Reachable','Unreachable')
+    print(tabulate(zip(ip_reachable,ip_unreachable),headers=headers))
+
+if __name__ == "__main__":
+    print_ip_table(ip_reachable,ip_unreachable)
+
