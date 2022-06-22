057f2d0182873e8f125be5370fe03f01cbcb4127
andcb22 <andcb@tut.by>
2022-06-22 16:55:07 +0300

    done 15_3 with test

--- exercises/15_module_re/task_15_3.py
+++ exercises/15_module_re/task_15_3.py
@@ -32,3 +32,70 @@ object network LOCAL_10.1.9.5
 
 Во всех правилах для ASA интерфейсы будут одинаковыми (inside,outside).
 """
+import re
+
+#filename="sh_ip_int_br.txt"
+filename="cisco_nat_config.txt"
+filename_out="cisco_asa_config.txt"
+data = [
+("R1", "12.4(24)T1", "Cisco 3825"),
+("R2", "15.2(2)T1", "Cisco 2911"),
+("SW1", "12.2(55)SE9", "Cisco WS-C2960-8TC-L"),
+]
+headers = ["hostname", "ios", "platform"]
+
+def convert_to_dict(headers, data):
+    data_list=[]
+    data_dict={}
+    for data_string in data:
+        for header in headers:
+            data_dict[header]=data_string[headers.index(header)]
+        data_list.append(data_dict)
+        data_dict={}
+    return(data_list)
+
+
+def parse_sh_ip_int_br(filename):
+    int_list=[]
+    intf=()
+    regex=(r'^(?P<INT>[a-zA-Z0-9/]+\d+)\s+(?P<IP>\S+)\s+(\w+)\s+(\w+)\s+(?P<STATUS>(administratively\s)*(up|down))\s+(?P<PROT>\S+)')
+    with open(filename) as f:
+        for line in f:
+            match = re.search(regex,line)
+            if match:
+                intf=(match.group('INT'),match.group('IP'),match.group('STATUS'),match.group('PROT'))
+                int_list.append(intf)
+    return(int_list)
+
+def convert_ios_nat_to_asa(filename,filename_out):
+    print('startinf func...')
+    template='''object network LOCAL_{}
+ host {}
+ nat (inside,outside) static interface service tcp {} {}
+'''
+    regex=(r'^ip nat inside source static tcp (?P<IP>\S+)\s+(?P<PIN>\d+)\s.+\s(?P<POUT>\d+)$')
+    with open(filename) as f:
+        with open(filename_out,'w') as f_out:
+            for line in f:
+#                print(line)
+                match = re.search(regex,line.rstrip())
+                if match:
+#                    print(match.group('IP')) 
+#                    print(match.group('PIN')) 
+#                    print(match.group('POUT')) 
+#                    print(template.format(match.group('IP'),match.group('IP'),match.group('PIN'),match.group('POUT')))
+                    f_out.write(template.format(match.group('IP'),match.group('IP'),match.group('PIN'),match.group('POUT')))
+    pass
+
+
+
+if __name__ == "__main__":
+
+#result = parse_sh_ip_int_br(filename)
+#result = convert_to_dict(headers, data)
+#if result:
+#   print(result)
+#else:
+#   print('...nothing...')
+    print('Starting main...')
+    convert_ios_nat_to_asa(filename,filename_out)
